315086d0a2165f0c1f7844ba10356398db593c1e
cfigueiroa <cfigueiroa@gmail.com>
2022-11-16 23:55:42 -0300

    feat: add profile image handler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Navbar from "./Navbar";
-import Corpo from "./Corpo";
-import FundoMobile from "./FundoMobile";
+import Navbar from "./components/Navbar";
+import Corpo from "./components/Corpo";
+import FundoMobile from "./components/FundoMobile";
 
 export default function App() {
     return (
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -3,19 +3,25 @@ import { IonIcon } from '@ionic/react';
 import { pencil } from 'ionicons/icons';
 
 export default function Usuario(props) {
+    
     const [nome, setNome] = useState(props.data.nome);
     function handleClick() {
         const novoNome = prompt("Qual o novo nome? Nome atual: " + nome);
         setNome(novoNome);
     }
+    const [foto, setFoto] = useState(`assets/img/${props.data.usuario}.svg`);
+    function handleImg(){
+        const novaImg = prompt("Qual a nova imagem? Imagem atual: " + foto);
+        setFoto(novaImg);
+    }
     return (
         <div className="usuario">
-            <img alt="" src={`assets/img/${props.data.usuario}.svg`} />
+            <img alt="" src={foto} onClick={handleImg} />
             <div className="texto">
                 <strong>{props.data.usuario}</strong>
                 <span>
                     {nome}
-                    <IonIcon icon={pencil} onclick={handleClick} />
+                    <IonIcon icon={pencil} onClick={handleClick} />
                 </span>
             </div>
         </div>
