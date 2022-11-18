6b5ed5695027dde72a59ce2ce0a0a4e94c5c99b5
cfigueiroa <cfigueiroa@gmail.com>
2022-11-18 12:23:33 -0300

    fix: check if nome or foto is falsy

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -7,14 +7,28 @@ export default function Usuario(props) {
     const [foto, setFoto] = useState(`assets/img/${props.data.usuario}.svg`);
     const [nome, setNome] = useState(props.data.nome);
 
+    function mudaNome() {
+        const temp = prompt("Qual o novo nome? Nome atual: " + nome);
+        if (temp) {
+            setNome(temp);
+        }
+    }
+
+    function mudaFoto() {
+        const temp = prompt("Qual a nova imagem? Imagem atual: " + foto);
+        if (temp) {
+            setFoto(temp);
+        }
+    }
+
     return (
         <div data-test="user" className="usuario">
-            <img data-test="profile-image" alt="" src={foto} onClick={() => setFoto(prompt("Qual a nova imagem? Imagem atual: " + foto))} />
+            <img data-test="profile-image" alt="" src={foto} onClick={mudaFoto} />
             <div className="texto">
                 <strong>{props.data.usuario}</strong>
                 <span data-test="name">
                     {nome}
-                    <IonIcon data-test="edit-name" icon={pencil} onClick={() => setNome(prompt("Qual o novo nome? Nome atual: " + nome))} />
+                    <IonIcon data-test="edit-name" icon={pencil} onClick={mudaNome} />
                 </span>
             </div>
         </div>
