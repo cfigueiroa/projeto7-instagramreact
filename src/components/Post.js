c9c45847e547ff2d474e8310d15843fcde55102f
cfigueiroa <cfigueiroa@gmail.com>
2022-11-19 12:48:30 -0300

    refactor: add line breaks

--- src/components/Post.js
+++ src/components/Post.js
@@ -14,7 +14,7 @@ export default function Post(props) {
         <div data-test="post" className="post" key={id}>
             <div className="topo">
                 <div className="usuario">
-                    <img alt="" src={`assets/img/${usuario}.svg`} />
+                    <img alt={usuario} src={`assets/img/${usuario}.svg`} />
                     {usuario}
                 </div>
                 <div className="acoes">
@@ -23,21 +23,35 @@ export default function Post(props) {
             </div>
             <div className="conteudo">
                 {hearts && <IonIcon icon={heart} className="heart" />}
-                <img data-test="post-image" onDoubleClick={() => !likeds ? (setLikeds(() => !likeds), setCurtidasCount(() => curtidasCount + 1), setHearts(() => true, setTimeout(() => setHearts(() => false), 500))) : setHearts(() => true, setTimeout(() => setHearts(() => false), 1000))} alt="" src={`assets/img/${img}.svg`} />
+                <img
+                    data-test="post-image"
+                    onDoubleClick={() => !likeds ? (setLikeds(() => !likeds), setCurtidasCount(() => curtidasCount + 1), setHearts(() => true, setTimeout(() => setHearts(() => false), 500))) : setHearts(() => true, setTimeout(() => setHearts(() => false), 500))}
+                    alt={img}
+                    src={`assets/img/${img}.svg`}
+                />
             </div>
             <div className="fundo">
                 <div className="acoes">
                     <div>
-                        <IonIcon data-test="like-post" onClick={() => !likeds ? (setLikeds(() => !likeds), setCurtidasCount(() => curtidasCount + 1)) : (setLikeds(() => !likeds), setCurtidasCount(() => curtidasCount - 1))} icon={likeds ? heart : heartOutline} style={likeds ? { color: "#ed4956" } : {}} />
+                        <IonIcon
+                            data-test="like-post"
+                            onClick={() => !likeds ? (setLikeds(() => !likeds), setCurtidasCount(() => curtidasCount + 1)) : (setLikeds(() => !likeds), setCurtidasCount(() => curtidasCount - 1))}
+                            icon={likeds ? heart : heartOutline}
+                            style={likeds ? { color: "#ed4956" } : {}}
+                        />
                         <IonIcon icon={chatbubbleOutline} />
                         <IonIcon icon={paperPlaneOutline} />
                     </div>
                     <div>
-                        <IonIcon data-test="save-post" onClick={() => setSaveds(() => !saveds)} icon={saveds ? bookmark : bookmarkOutline} />
+                        <IonIcon
+                            data-test="save-post"
+                            onClick={() => setSaveds(() => !saveds)}
+                            icon={saveds ? bookmark : bookmarkOutline}
+                        />
                     </div>
                 </div>
                 <div className="curtidas">
-                    <img alt="" src={`assets/img/${curtidopor}.svg`} />
+                    <img alt={curtidopor} src={`assets/img/${curtidopor}.svg`} />
                     <div className="texto">
                         Curtido por <strong>{curtidopor}</strong> e <strong data-test="likes-number">outras {curtidasCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")} pessoas</strong>
                     </div>
